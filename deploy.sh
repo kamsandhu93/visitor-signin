8ce12e7298636c8d0875d0cb515bfbc8d5d81c29
xLasercut <tianxun.wang2004@gmail.com>
2018-09-21 12:03:56 +0100

    added command line options for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-db_dir=/opt/visitor-db/
-db_file=visitor_db.db
+db_dir="/opt/visitor-db/"
+db_file="visitor_db.db"
 
 # ensure running as root
 if [ "$(id -u)" != "0" ]; then
@@ -16,4 +16,33 @@ if [ ! -f $db_file ]; then
     cp $db_file $db_dir
 fi
 
-docker-compose up --build
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -b|--build)
+        FORCE_BUILD=true
+        shift
+        ;;
+        -d|--debug)
+        DEBUG=true
+        shift
+        ;;
+        *)
+        shift
+        ;;
+    esac
+done
+
+cmd="docker-compose up"
+
+if [ "$FORCE_BUILD" = true ]; then
+    cmd+=" --build"
+fi
+
+if [ ! "$DEBUG" = true ]; then
+    cmd+=" -d"
+fi
+
+$cmd
