aec622a7452b2210bc37c37ed8b7d816759caf75
xLasercut <tianxun.wang2004@gmail.com>
2018-09-21 11:32:13 +0100

    deploy script no longer tries to create and copy db file if there is existing file already

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-sudo mkdir /opt/visitor-db
-sudo cp visitor_db.db /opt/visitor-db/
-sudo docker-compose up --build
+
+db_dir=/opt/visitor-db/
+db_file=visitor_db.db
+
+# ensure running as root
+if [ "$(id -u)" != "0" ]; then
+  exec sudo "$0" "$@"
+fi
+
+if [ ! -d $db_dir ]; then
+    mkdir $db_dir
+fi
+
+if [ ! -f $db_file ]; then
+    cp $db_file $db_dir
+fi
+
+docker-compose up --build
