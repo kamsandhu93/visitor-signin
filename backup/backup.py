d8fc46cc3997621efef54cc9417c5f2c1c4c6efa
xLasercut <tianxun.wang2004@gmail.com>
2018-09-24 15:11:41 +0100

    added restore functionality to backup script

--- backup/backup.py
+++ backup/backup.py
@@ -8,12 +8,96 @@ import os
 from dropbox.exceptions import ApiError, AuthError
 from dropbox.files import WriteMode
 
-def startLogging(logFilePath):
+class ArgParser(object):
+    """
+    Class to parse arguments
+    """
+
+    def _checkFile(value):
+        """
+        Check file exists and is not directory
+        """
+        if not os.path.isabs(value):
+            raise argparse.ArgumentTypeError("{0} is not an absolute path".format(value))
+
+        if not os.path.exists(value):
+            raise argparse.ArgumentTypeError("{0} does not exist".format(value))
+
+        if not os.path.isfile(value):
+            raise argparse.ArgumentTypeError("{0} is not a file path".format(value))
+
+        return value
+
+    def _checkDirectory(value):
+        """
+        Check path is directory and exists
+        """
+        if not os.path.isabs(value):
+            raise argparse.ArgumentTypeError("{0} is not an absolute path".format(value))
+
+        if not os.path.exists(value):
+            raise argparse.ArgumentTypeError("{0} does not exist".format(value))
+
+        if os.path.isfile(value):
+            raise argparse.ArgumentTypeError("{0} is not a directory".format(value))
+
+        return value
+
+    def _checkOperation(value):
+        """
+        Check input operation is valid
+        """
+        validOperations = ["backup", "restore", "restore_force"]
+        if value not in validOperations:
+            raise argparse.ArgumentTypeError("{0} is not in a valid operation".format(value))
+
+        return value
+
+    def parseArgs(self, args):
+        """
+        Parse arguments
+        """
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument(
+            "-t", "--token",
+            type=str, required=True,
+            help="Dropbox api token"
+        )
+
+        parser.add_argument(
+            "-p", "--path",
+            type=self._checkDirectory, required=True,
+            help="Database directory path"
+        )
+
+        parser.add_argument(
+            "-l", "--log",
+            type=self._checkDirectory, required=True,
+            help="Log directory path"
+        )
+
+        parser.add_argument(
+            "-f", "--file",
+            type=str, required=True,
+            help="Database file name"
+        )
+
+        parser.add_argument(
+            "-o", "--operation",
+            type=self._checkOperation, required=True,
+            help="Backup or restore database"
+        )
+
+        arguments = parser.parse_args(args)
+        return arguments
+
+def startLogging(logFilePath, operation):
     """
     Logging configurations
     """
 
-    loggingFormat = logging.Formatter("%(asctime)s | Backup | %(levelname)s | %(message)s")
+    loggingFormat = logging.Formatter("%(asctime)s | {0} | %(levelname)s | %(message)s".format(operation))
 
     fileHandler = logging.FileHandler(logFilePath, encoding = "utf-8")
     fileHandler.setFormatter(loggingFormat)
@@ -25,77 +109,11 @@ def startLogging(logFilePath):
     logging.root.addHandler(fileHandler)
     logging.root.addHandler(consoleHandler)
 
-def checkFile(value):
+
+def backup(dbx, dbPath, backupName):
     """
-    Check file exists and is not directory
+    backup database
     """
-    if not os.path.isabs(value):
-        raise argparse.ArgumentTypeError("{0} is not an absolute path".format(value))
-
-    if not os.path.exists(value):
-        raise argparse.ArgumentTypeError("{0} does not exist".format(value))
-
-    if not os.path.isfile(value):
-        raise argparse.ArgumentTypeError("{0} is not a file path".format(value))
-
-    return value
-
-def checkDirectory(value):
-    """
-    Check path is directory and exists
-    """
-    if not os.path.isabs(value):
-        raise argparse.ArgumentTypeError("{0} is not an absolute path".format(value))
-
-    if not os.path.exists(value):
-        raise argparse.ArgumentTypeError("{0} does not exist".format(value))
-
-    if os.path.isfile(value):
-        raise argparse.ArgumentTypeError("{0} is not a directory".format(value))
-
-    return value
-
-def argParser(args):
-    """
-    Parse arguments
-    """
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "-t", "--token",
-        type=str, required=True,
-        help="Dropbox api token"
-    )
-
-    parser.add_argument(
-        "-p", "--path",
-        type=checkFile, required=True,
-        help="Database file path"
-    )
-
-    parser.add_argument(
-        "-l", "--log",
-        type=checkDirectory, required=True,
-        help="Log file path"
-    )
-
-    arguments = parser.parse_args(args)
-    return arguments
-
-
-def main():
-    """
-    Main
-    """
-    arguments = argParser(sys.argv[1:])
-
-    dbPath = arguments.path
-    backupName = "/{0}".format(os.path.split(arguments.path)[1])
-
-    startLogging(os.path.join(arguments.log, "backup.log"))
-
-    dbx = dropbox.Dropbox(arguments.token)
-
     with open (dbPath, "rb") as dbFile:
         try:
             dbx.files_upload(dbFile.read(), backupName, mode=WriteMode("overwrite"))
@@ -111,6 +129,39 @@ def main():
             else:
                 logging.err(err)
                 sys.exit()
+
+def restore(dbx, dbPath, backupName, force=None):
+    """
+    restore database
+    """
+    if force:
+        dbx.files_download_to_file(dbPath, backupName)
+    elif not os.path.exists(dbPath):
+        dbx.files_download_to_file(dbPath, backupName)
+
+
+def main():
+    """
+    Main
+    """
+    argParser = ArgParser()
+    arguments = argParser.parseArgs(sys.argv[1:])
+
+    dbPath = os.path.join(arguments.path, arguments.file)
+    backupName = "/{0}".format(arguments.file)
+
+    dbx = dropbox.Dropbox(arguments.token)
+
+    operations = {
+        "backup": backup(dbx, dbPath, backupName),
+        "restore": restore(dbx, dbPath, backupName),
+        "restore_force": restore(dbx, dbPath, backupName, True)
+    }
+
+    startLogging(os.path.join(arguments.log, "backup.log"))
+
+    operations[arguments.operation]
+
 
 if __name__ == "__main__":
     main()
