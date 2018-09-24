7c06b69b3a127fc8cbef365b96bc878ddfd22bff
xLasercut <tianxun.wang2004@gmail.com>
2018-09-24 10:03:19 +0100

    added some changes to fix flask app import errors

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 db_dir="/opt/visitor-db/"
-db_file="visitor_db.db"
 
 # ensure running as root
 if [ "$(id -u)" != "0" ]; then
@@ -12,10 +11,7 @@ if [ ! -d $db_dir ]; then
     mkdir $db_dir
 fi
 
-if [ ! -f $db_file ]; then
-    cp $db_file $db_dir
-fi
-
+#parse command line options
 while [[ $# -gt 0 ]]
 do
     key="$1"
@@ -29,6 +25,11 @@ do
         DEBUG=true
         shift
         ;;
+        -t|--token)
+        TOKEN=$2
+        shift
+        shift
+        ;;
         *)
         shift
         ;;
@@ -37,12 +38,18 @@ done
 
 cmd="docker-compose up"
 
+#build command to run
 if [ "$FORCE_BUILD" = true ]; then
     cmd+=" --build"
 fi
 
 if [ ! "$DEBUG" = true ]; then
     cmd+=" -d"
+fi
+
+if [ -z "$TOKEN" ]; then
+    echo "Did not supply dropbox token"
+    exit 1
 fi
 
 $cmd
--- flask_app/visitor_app/__init__.py
+++ flask_app/visitor_app/__init__.py
@@ -2,8 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from flask_app.visitor_app.handlers import view, login_handler, logout_handler, status_handler
-
-
-
-
+from visitor_app.handlers import view, login_handler, logout_handler, status_handler
