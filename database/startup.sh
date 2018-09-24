293f6ed4473324dd4a1a0a296ce1a2fa6397497d
xLasercut <tianxun.wang2004@gmail.com>
2018-09-24 12:12:34 +0100

    added startup script to start cronjob

---
+++ database/startup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function checkInput () {
+    if [ -z "$1" ]; then
+        echo "$2"
+        exit 1
+    fi
+}
+
+#parse command line options
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -h|--host)
+        HOST="$2"
+        shift
+        shift
+        ;;
+        -p|--port)
+        PORT="$2"
+        shift
+        shift
+        ;;
+        -d|--database)
+        DB_FILE="$2"
+        shift
+        shift
+        ;;
+        *)
+        shift
+        ;;
+    esac
+done
+
+checkInput "$HOST" "Host not found"
+checkInput "$PORT" "Port not found"
+checkInput "$DB_FILE" "Database file not found"
+
+crond -b
+sqlite_web -H "$HOST" -p "$PORT" "$DB_FILE"
