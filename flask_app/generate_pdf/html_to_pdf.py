e4c6847c5878d875c190a77417877d12478c0514
David Indrawes <david.indrawes@nhs.net>
2018-10-03 10:05:26 +0100

    wip trying to work around printing html file in headless container

--- flask_app/generate_pdf/html_to_pdf.py
+++ flask_app/generate_pdf/html_to_pdf.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-
 # ToDo review security for the following packages
-import pdfkit
 from bs4 import BeautifulSoup
 
 
@@ -25,20 +23,31 @@ def generate_pass(pass_id, name, company=None):
     company_element.string.replace_with(company)
     date_element = soup.find(id="date")
     # ToDO do we need to pick this date from the DB ? or something 
-    date_element.string.replace_with(datetime.now().strftime("%d-%M-%Y"))
+    date_element.string.replace_with(datetime.now().strftime("%d-%m-%Y"))
     pass_id_element = soup.find(id="passid")
     pass_id_element.string.replace_with(pass_id_element.string + pass_id)
 
     with open("./generate_pdf/generated.html", "w") as out:
         out.write(str(soup))
 
+    import pdfkit
 
-    pdf_options = {
-        'page-size':'A4',
-        'encoding':'utf-8', 
-        'margin-top':'0cm',
-        'margin-bottom':'0cm',
-        'margin-left':'0cm',
-        'margin-right':'0cm'
-    }
-    pdfkit.from_file('./generate_pdf/generated.html', 'test.pdf', options=pdf_options)
+    class HeadlessPdfKit(pdfkit.PDFKit):
+        def command(self, path=None):
+            return ['xvfb-run', '--'] + super().command(path)
+
+    with open("./generate_pdf/generated.html") as generated:
+        txt = generated.read()
+        pdf = HeadlessPdfKit(txt, 'string').to_pdf(False)
+        f = open('lol.pdf', 'w+b')
+        f.write(pdf)
+    # pdf_options = {
+    #     'page-size':'A4',
+    #     'margin-top':'0cm',
+    #     'margin-bottom':'0cm',
+    #     'margin-left':'0cm',
+    #     'margin-right':'0cm'
+    # }
+    # pdfkit.from_file('./generate_pdf/generated.html', 'test.pdf', options=pdf_options)
+    # import imgkit
+    # imgkit.from_file('generated.html', 'out.jpg', options={"xvfb": "", "quality":100})
