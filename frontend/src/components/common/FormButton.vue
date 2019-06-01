3dfcee80de31a49ebdf2fb01516b5008cb4803f4
xLasercut <tianxun.wang2004@gmail.com>
2019-06-01 11:44:40 +0100

    added btn disable function to sign out page too

--- frontend/src/components/common/FormButton.vue
+++ frontend/src/components/common/FormButton.vue
@@ -1,13 +1,24 @@
 <template>
     <nhs-row>
         <nhs-col :span="33">
-            <nhs-button @click="$emit('submitForm')" name="confirm-button">Confirm</nhs-button>
+            <nhs-button @click="$emit('submitForm')" name="confirm-button" :disabled="disabled">Confirm</nhs-button>
         </nhs-col>
         <nhs-col :span="33">
-            <nhs-button color="secondary" @click="$emit('resetForm')" name="reset-button">Reset</nhs-button>
+            <nhs-button color="secondary" @click="$emit('resetForm')" name="reset-button" :disabled="disabled">Reset</nhs-button>
         </nhs-col>
         <nhs-col :span="33">
-            <nhs-button color="secondary" @click="$router.push({path: '/'})" name="cancel-button">Cancel</nhs-button>
+            <nhs-button color="secondary" @click="$router.push({path: '/'})" name="cancel-button" :disabled="disabled">Cancel</nhs-button>
         </nhs-col>
     </nhs-row>
 </template>
+
+<script>
+    export default {
+        props: {
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
