22325258712f081c7428d113d5a940509b7d97f6
xLasercut <tianxun.wang2004@gmail.com>
2019-06-06 20:23:07 +0100

    reverted frontend build

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+function loadNvm() {
+    if [[ -d ~/.nvm ]]; then
+        echo "Node install from NVM found. Using NVM installed node"
+        export NVM_DIR=~/.nvm
+        source ~/.nvm/nvm.sh
+    fi
+}
+
+function buildFrontend() {
+    loadNvm
+    pushd ./frontend
+    npm install
+    npm run build
+    popd
+}
+
 while getopts ":s: :hn" opt; do
     case ${opt} in
         h )
@@ -29,12 +45,16 @@ shift $((OPTIND -1))
 
 command="sudo docker-compose build"
 
-if [[ $noCache = true ]]; then
+if [[ ! -z $noCache ]]; then
     command+=" --no-cache"
 fi
 
 if [[ -z $container ]]; then
+    buildFrontend
     $command
 else
+    if [[ $container = "frontend" ]]; then
+        buildFrontend
+    fi
     $command $container
 fi
